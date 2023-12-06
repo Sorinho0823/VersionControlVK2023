512bf9af284605ae59a5dc0d29663eebeb795baa
Sorinho0823 <adam.soros@stu.uni-corvinus.hu>
2023-12-06 14:27:07 +0100

    példányosítás

--- Week5/Form1.cs
+++ Week5/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Week5.MnbServiceReference;
 
 namespace Week5
 {
@@ -15,6 +16,18 @@ namespace Week5
         public Form1()
         {
             InitializeComponent();
+
+            var mnbService = new MNBArfolyamServiceSoapClient();
+
+            var request = new GetExchangeRatesRequestBody()
+            {
+                currencyNames = "EUR",
+                startDate = "2020-01-01",
+                endDate = "2020-06-30"
+            };
+
+            var response = mnbService.GetExchangeRates(request);
+            var result = response.GetExchangeRatesResult;
         }
     }
 }
