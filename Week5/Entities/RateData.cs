9c53c231e96e8171b85457d902170a5fc82cee72
Sorinho0823 <adam.soros@stu.uni-corvinus.hu>
2023-12-06 14:34:39 +0100

    RateData osztaly letrehozasa

---
+++ Week5/Entities/RateData.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week5.Entities
+{
+    class RateData
+    {
+        public DateTime Date { get; set; }
+        public string Currency { get; set; }
+        public decimal Value { get; set; }
+    }
+}
--- Week5/Form1.cs
+++ Week5/Form1.cs
@@ -13,6 +13,8 @@ namespace Week5
 {
     public partial class Form1 : Form
     {
+
+        
         public Form1()
         {
             InitializeComponent();
