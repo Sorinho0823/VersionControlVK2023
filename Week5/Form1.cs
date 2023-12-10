af77dd60e1346369e928de917d02ca01df5769d9
Sorinho0823 <adam.soros@stu.uni-corvinus.hu>
2023-12-10 14:06:18 +0100

    4es feladat

--- Week5/Form1.cs
+++ Week5/Form1.cs
@@ -7,17 +7,19 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Week5.Entities;
 using Week5.MnbServiceReference;
 
 namespace Week5
 {
     public partial class Form1 : Form
     {
-
+        BindingList<RateData> Rates = new BindingList<RateData>();
         
         public Form1()
         {
             InitializeComponent();
+            dataGridView1.DataSource = Rates;
 
             var mnbService = new MNBArfolyamServiceSoapClient();
 
